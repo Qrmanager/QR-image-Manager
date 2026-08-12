4b9d21ec3fe23bd2f7f3e8f5e9102f211ec42e6a
Qrmanager <minteabe17@gmail.com>
2026-08-12 11:33:59 +0300

    Prepare app for deployment

--- server.js
+++ server.js
@@ -188,6 +188,8 @@ app.post("/upload", upload.single("image"), (req, res) => {
 });
 
 // Start server
-app.listen(3000, () => {
-    console.log("Server running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
